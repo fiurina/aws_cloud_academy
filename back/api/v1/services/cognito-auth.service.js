7b90e2cabf873a0ef9c1f035fdfa6f108ca3fe87
Gabriel Bové <gabrielbove12@gmail.com>
2023-01-19 13:09:29 +0100

    Base project

--- back/api/v1/services/cognito-auth.service.js
+++ back/api/v1/services/cognito-auth.service.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk');
 
 var awsRegion = "eu-west-1";
-//TODO:Replace this identity with a correct one
-var identityPoolId = "eu-west-1:2963b769-4184-47je-b57e-57f7397cdd26";
+//TODO:Add identity pool
+var identityPoolId = "";
 const AWSService = AWS;  
 authenticate();
 
